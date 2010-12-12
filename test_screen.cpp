232f5ace25b90faa8a746dea0e89dc2769cbd998
Mickaël Wolff <mickael@lupusmic.org>
2010-12-12 19:32:19 +0000

    Add boxes in the screen

--- test_screen.cpp
+++ test_screen.cpp
@@ -18,8 +18,13 @@ int main(int argc, char **argv)
 		<< std::endl ;
 
 	Surface s ;
-	screen.create(s, create_videomode(20, 20, 16)) ;
-	s.set_background(0xffff) ;
+
+	screen.create(s, create_videomode(50, 50, 16)) ;
+	s.set_background(0x00ff00) ;
+	screen.draw(s) ;
+
+	screen.create(s, create_videomode(30, 30, 16)) ;
+	s.set_background(0xffff00) ;
 	screen.draw(s) ;
 
 	screen.update() ;
