3ab2e0f9d7b7e0af7a862e7c521ec8cd3a40c68d
Mickaël Wolff <mickael@lupusmic.org>
2012-08-02 22:36:58 +0200

    We need to set the screen style (size) before we actually init

--- screen.cpp
+++ screen.cpp
@@ -13,6 +13,10 @@ Screen::SharedPtr Screen::make(Gui & gui)
 Screen::Screen(Gui & gui)
 	: Box {gui}
 {
+	Style s = style() ;
+	s.size(gui.layout().videomode().size()) ;
+	assert(s.size().width() != 0) ;
+	style(s) ;
 }
 
 Screen::~Screen()
@@ -24,6 +28,7 @@ void Screen::init()
 	auto p_s = gui().layout().screen() ;
 	Style s = style() ;
 	s.size(p_s->videomode().size()) ;
+	assert(s.size().width() != 0) ;
 	style(s) ;
 	surface(std::move(p_s)) ;
 }
